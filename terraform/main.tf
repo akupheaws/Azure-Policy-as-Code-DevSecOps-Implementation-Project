87757185360df17ca37814ff07b53988d2bbc3f3
Akuphe Dieudonne <akupheaws@gmail.com>
2025-09-17 12:55:03 -0500

    updated tf-opa file

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,7 +137,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-########################################
+#######################################
 # 8 Linux VMs
 ########################################
 resource "azurerm_linux_virtual_machine" "vm" {
